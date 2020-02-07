e63f250d02f7f069b048cf5932e9af454e6edc57
Kristine Bujold <kristine.bujold@windriver.com>
2020-02-07 09:00:39 -0500

    Fix bugs under Software Management panels
    Fix table row update and page links.
    
    Also apply post/redirect to eliminate form resubmit at browser reload
    on DC software management page. This relates to bug
    https://bugs.launchpad.net/starlingx/+bug/1842747. It should of been
    applied to the DC panels as well.
    
    Story: 2007255
    Task: 38664
    Change-Id: I471cc7bd986dfb305e90930c6397ee997a04c5b7
    Signed-off-by: Kristine Bujold <kristine.bujold@windriver.com>

--- starlingx-dashboard/starlingx-dashboard/starlingx_dashboard/api/dc_manager.py
+++ starlingx-dashboard/starlingx-dashboard/starlingx_dashboard/api/dc_manager.py
@@ -10,7 +10,7 @@
 #  License for the specific language governing permissions and limitations
 #  under the License.
 #
-# Copyright (c) 2017-2018 Wind River Systems, Inc.
+# Copyright (c) 2017-2020 Wind River Systems, Inc.
 #
 
 import logging
@@ -129,6 +129,12 @@ def step_list(request):
     response = dcmanagerclient(request).strategy_step_manager.\
         list_strategy_steps()
     return [Step(step) for step in response]
+
+
+def step_detail(request, cloud_name):
+    response = dcmanagerclient(request).strategy_step_manager. \
+        strategy_step_detail(cloud_name)
+    return Step(response[0])
 
 
 class Config(base.APIResourceWrapper):
