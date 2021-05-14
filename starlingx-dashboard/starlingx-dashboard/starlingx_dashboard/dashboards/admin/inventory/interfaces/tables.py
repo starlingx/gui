e05e1a43531499d94cfb1e538683ee36eea92b43
Teresa Ho <teresa.ho@windriver.com>
2021-05-13 23:04:04 -0400

    Do not display primary_reselect if not specified
    If the attribute 'primary_reselect' is not specified, the sysinv API
    will leave it as null and GUI will not display the attribute.
    
    Closes-Bug: 1928461
    
    Change-Id: I5b8ef8b29fb7775dde8607bb14cd733015269f82
    Signed-off-by: Teresa Ho <teresa.ho@windriver.com>

--- starlingx-dashboard/starlingx-dashboard/starlingx_dashboard/dashboards/admin/inventory/interfaces/tables.py
+++ starlingx-dashboard/starlingx-dashboard/starlingx_dashboard/dashboards/admin/inventory/interfaces/tables.py
@@ -138,7 +138,8 @@ def get_attributes(interface):
         if interface.aemode in ['balanced', '802.3ad']:
             attr_str = "%s, AE_XMIT_HASH_POLICY=%s" % (
                 attr_str, interface.txhashpolicy)
-        elif interface.aemode == 'active_standby':
+        elif (interface.aemode == 'active_standby' and
+                interface.primary_reselect):
             attr_str = "%s, primary_reselect=%s" % (
                 attr_str, interface.primary_reselect)
     if interface.ifclass and interface.ifclass == 'data':
