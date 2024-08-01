fa986e4fb9036163d62d84731778a6f3550cb2b5
Italo Lemos <Italo.doRegoLemos@windriver.com>
2024-08-01 18:50:27 -0300

    Add subcloud's endpoints into request context
    This change retrieves subcloud endpoints from
    the dc_manager API and populate the service
    catalog through the request context.
    
    TEST PLAN:
    [PASS] Build, install.
    [PASS] Able to switch to subcloud regions.
    [PASS] Verify the usage of Horizon without subcloud endpoints in the database.
    [PASS] Without Keystone subcloud endpoints in the database:
           	- Navigate to Distributed Cloud Admin -> Cloud OverView ->
              Subclouds Table -> Alarm & Event Details.
           	- Access and check subcloud Active Alarm page from subcloud
              perspective (Admin -> Fault Management -> Active Alarms).
           	- Access and check subcloud Events page from subcloud
              perspective (Admin -> Fault Management -> Events).
           	- Access and check subcloud Events Suppression page from
              subcloud perspective (Admin -> Fault Management -> Events
              Suppression).
    
    [PASS] Without Keystone subcloud endpoints in the database:
            - Navigate to Distributed Cloud Admin -> Cloud OverView ->
              Subclouds Table -> Host Details.
            - Access and check Host Inventory page from subcloud perspective
              (Admin -> Platform -> Host Inventory).
            - Access and check Software Management page from subcloud
              perspective. (Admin -> Platform -> Host Software Management).
    
    Story: 2010676
    Task: 50575
    
    Change-Id: I102de87e9591a75a7e86c0a3be1a0b1d5cb94687
    Signed-off-by: Italo Lemos <Italo.doRegoLemos@windriver.com>

--- starlingx-dashboard/starlingx-dashboard/starlingx_dashboard/dashboards/dc_admin/cloud_overview/views.py
+++ starlingx-dashboard/starlingx-dashboard/starlingx_dashboard/dashboards/dc_admin/cloud_overview/views.py
@@ -1,13 +1,80 @@
 #
-# Copyright (c) 2017-2020 Wind River Systems, Inc.
+# Copyright (c) 2017-2024 Wind River Systems, Inc.
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
+import uuid
 
+from django.utils.translation import ugettext_lazy as _
+
+from horizon import exceptions
 from horizon import views
+from openstack_dashboard.api import base
+
+from starlingx_dashboard import api as stx_api
+
+
+def _get_service_port(url):
+    url_list = url.split('://')
+    ip = url_list[-1]
+
+    return ip.split(':')[-1]
+
+
+def _build_endpoint(endpoint_url, subcloud_ip, region_name):
+    service_port = _get_service_port(endpoint_url)
+
+    new_endpoint = {
+        "id": str(uuid.uuid4()),
+        "interface": "admin",
+        "region_id": region_name,
+        "url": f"https://{subcloud_ip}:{service_port}",
+        "region": region_name,
+    }
+
+    return new_endpoint
 
 
 class IndexView(views.HorizonTemplateView):
     template_name = 'dc_admin/cloud_overview/index.html'
     page_title = 'Cloud Overview'
+
+    def get_subclouds(self):
+        try:
+            return stx_api.dc_manager.subcloud_list(self.request)
+        except Exception:
+            exceptions.handle(self.request,
+                              _('Unable to retrieve subclouds.'))
+            return None
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        subclouds = self.get_subclouds()
+
+        if subclouds:
+            subcloud_service_types = [
+                "platform",
+                "faultmanagement",
+                "identity",
+                "nfv",
+                "usm",
+                "patching"
+            ]
+            for subcloud in subclouds:
+                for service in self.request.user.service_catalog:
+                    if service["type"] in subcloud_service_types:
+                        endpoint = base.url_for(
+                            self.request,
+                            service["type"],
+                            endpoint_type="admin",
+                            region="RegionOne"
+                        )
+                        service["endpoints"].append(_build_endpoint(
+                            endpoint,
+                            subcloud.management_start_ip,
+                            subcloud.region_name)
+                        )
+
+        return context
