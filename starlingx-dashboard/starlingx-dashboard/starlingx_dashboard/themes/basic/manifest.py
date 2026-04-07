4c29d4d281815afab177d7be101532505db5421e
Felipe Vasconcelos <Felipe.Vasconcelos@windriver.com>
2026-04-07 11:24:43 -0300

    Adding basic theme
    Improved process of customizing Horizon branding by adding 'basic',
    a new theme serving as more relevant example for current
    implementation which :
      - includes necessary files and directories for easy customization
      - allows png as standard for custom images instead of svg/ico
    
    Test Plan:
    PASS: Build starlingx-dashboard package, confirm themes/basic/ is
    at /usr/lib/python3/dist-packages/starlingx_dashboard/themes/basic/
    PASS: Copy basic theme from installed path, customize png files,
    create .tgz tarball, deploy to /opt/branding/, restart Horizon
    PASS: Verify custom branding renders on login and navbar
    PASS: Package build ships themes/basic at
    /usr/share/openstack-dashboard/themes/basic/ alongside default,
    example, material, and starlingx themes
    
    Note: Usage is documented in the companion docs review (994153).
    
    Closes-bug: 2147454
    
    Change-Id: I57bdde193bc8c3ea57cac5805d2d0ba8e37308cc
    Signed-off-by: Felipe Vasconcelos <Felipe.Vasconcelos@windriver.com>

---
+++ starlingx-dashboard/starlingx-dashboard/starlingx_dashboard/themes/basic/manifest.py
@@ -0,0 +1,9 @@
+#
+# Copyright (c) 2026 Wind River Systems, Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+SITE_BRANDING = "Company Name Example"
+
+# HORIZON_CONFIG["help_url"] = "https://www.openstack.org"
